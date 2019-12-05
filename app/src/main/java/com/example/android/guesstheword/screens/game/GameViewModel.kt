24f6c493c48796326ba27243ffa63ddee600d359
JingMingChan <prochan75103161@gmail.com>
2019-12-05 12:35:15 +0800

    5.2 GuessTheWord-Starter ver 2

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,18 +1,30 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
-    // The current word
-    var word = ""
-    // The current score
-    var score = 0
-    // The list of words - the front of the list is the next word to guess
+
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    private val _score =  MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
     lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
+        _word.value = ""
+        _score.value = 0
         resetList()
         nextWord()
     }
@@ -22,16 +34,17 @@ class GameViewModel:ViewModel() {
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 
+
     fun onSkip() {
         if (!wordList.isEmpty()) {
-            score--
+            _score.value = (score.value)?.minus(1)
         }
         nextWord()
     }
 
     fun onCorrect() {
         if (!wordList.isEmpty()) {
-            score++
+            _score.value = (score.value)?.plus(1)
         }
         nextWord()
     }
@@ -64,9 +77,18 @@ class GameViewModel:ViewModel() {
     }
 
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word = wordList.removeAt(0)
+        if (wordList.isEmpty()) {
+
+            onGameFinish()
+        }else{
+            _word.value = wordList.removeAt(0)
         }
+    }
+    fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
     }
 }
